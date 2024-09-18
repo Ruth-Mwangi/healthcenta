b19c661c5c516a37353c2f27f1253d592b981254
Ruth-Mwangi <rwmwangi96@gmail.com>
2024-09-18 12:04:46 +0300

    updated client.py to check if  postman proxy server is up or not.

--- src/api/client.py
+++ src/api/client.py
@@ -3,11 +3,17 @@ import os
 
 import requests
 
+def is_proxy_up(proxy_url):
+    """Check if the proxy server is reachable."""
+    try:
+        response = requests.get('http://www.google.com', proxies={'http': proxy_url, 'https': proxy_url}, timeout=5)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
 
 def post_data_to_api(data_file_path: str, api_url: str):
     postman_http_proxy = os.getenv('POSTMAN_HTTP_PROXY')
     postman_https_proxy = os.getenv('POSTMAN_HTTPS_PROXY')
-    postman_cert_path = os.getenv('POSTMAN_CERT')
     try:
         with open(data_file_path, 'r') as file:
             data = json.load(file)
@@ -21,10 +27,17 @@ def post_data_to_api(data_file_path: str, api_url: str):
     headers = {
     'Content-Type': 'application/json'
     }
-    proxies = {
-    'http': postman_http_proxy,
-    'https': postman_https_proxy
-}
+  
+    proxies = {}
+    if postman_http_proxy and postman_https_proxy:
+        # Check if the Postman proxy is up
+        if is_proxy_up(postman_http_proxy):
+            proxies = {
+                'http': postman_http_proxy,
+                'https': postman_https_proxy
+            }
+        else:
+            print("Postman proxy server is not up. Sending request without proxy.")
 
     try:
         response = requests.post(api_url, json=data,headers=headers,proxies=proxies,verify=False)
@@ -40,6 +53,6 @@ def post_data_to_api(data_file_path: str, api_url: str):
 
         return response_data
     
-    except Exception as e :
-        print("Error posting data:{e}")
+    except requests.RequestException as e :
+        print("Error posting data:",e)
         return
